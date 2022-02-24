d2fa7f874b6043bbb52b6faa352e137207a04c53
Ghost Mac <theghostmac@gmail.com>
2022-02-24 01:46:17 +0100

    implemented Proof Of Work

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,1 +1,45 @@
 package blockchain
+
+import (
+	"bytes"
+	"encoding/binary"
+	"log"
+	"math/big"
+)
+
+// Difficulty takes data from the block
+const Difficulty = 12
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int // requirement
+}
+
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty)) // left shift
+
+	PoW := &ProofOfWork{b, target}
+
+	return PoW
+}
+
+func (PoW *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			PoW.Block.PreviousHash,
+			PoW.Block.Data,
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func ToHex(num int64) []byte {
+	buffer := new(bytes.Buffer)
+	err := binary.Write(buffer, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buffer.Bytes()
+}
