bab8af797e7d2deae95f5a38b670124c42b8c68f
MacBobby Chibuzor <theghostmac@gmail.com>
2022-07-20 13:13:21 +0100

    created serialize and deserialize methods, with handle function for errors

--- blockchain/block.go
+++ blockchain/block.go
@@ -38,6 +38,7 @@ func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
+// Serialize the blockchain data structure into a slice of bytes for the BadgerDB to persist
 func (b *Block) Serialize() []byte {
 	var resultByteBuffer bytes.Buffer
 	encoder := gob.NewEncoder(&resultByteBuffer)
@@ -48,4 +49,23 @@ func (b *Block) Serialize() []byte {
 		log.Panic(err)
 	}
 	return resultByteBuffer.Bytes()
+}
+
+// Deserialize a slice of byte data and output a pointer to a Block
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
+}
+
+// Handle function to handle all errors
+func ()  {
+	
 }
